17a9938f6e648293d8764c0b7fbd5527050d0fd4
chanBroK <koc0819@gmail.com>
2022-04-19 17:40:19 +0900

    add get datasource endpoint:

---
+++ scan_controller/Controllers/ScanController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NTwain;
+using scan_controller.Service;
+
+namespace scan_controller.Controllers
+{
+    // 해당 컨트롤러가 호출될때 객체가 생성된다.
+    [RoutePrefix("api/scan")]
+    public class ScanController : ApiController
+    {
+        private static ScanService _scanService;
+        
+        public ScanController()
+        {
+            if (_scanService == null)
+            {
+                _scanService = new ScanService();
+            }
+        }
+        [Route("datasource")]
+        [HttpGet]
+        public List<string> GetDatasource()
+        {
+            try
+            {
+                var sourceList = _scanService.getDataSourceList();
+                var sourceNameList = new List<string>();
+                foreach (DataSource ds in sourceList)
+                {
+                    sourceNameList.Add(ds.Name);
+                }
+                return sourceNameList;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+        [Route("datasource/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage SetDatasource(int id)
+        {
+            Console.WriteLine(id);
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+}
---
+++ scan_controller/Service/ScanService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Configuration;
+using NTwain;
+using NTwain.Data;
+using SixLabors.ImageSharp;
+
+
+namespace scan_controller.Service
+{
+    public class ScanService
+    {
+        private static TwainSession _session;
+
+        private static string _savePath = "D://test.png";
+        
+        
+        public ScanService()
+        {
+            if (_session == null)
+            {
+                initSession();
+            }
+        }
+
+        private void initSession()
+        {
+            Console.WriteLine(PlatformInfo.Current.IsApp64Bit ? "Server Running on 64bit" : "Server Running on 32Bit");
+            // Set NTwain read twain_32.dll
+            PlatformInfo.Current.PreferNewDSM = false;
+            Console.WriteLine("Loaded DSM =" + PlatformInfo.Current.ExpectedDsmPath);
+            // Create Twain Session
+            _session = new TwainSession(TWIdentity.CreateFromAssembly(DataGroups.Image,
+                Assembly.GetExecutingAssembly()));
+            // Session 상태 별 handler 설정
+            
+            _session.TransferReady += (s, e) => { Console.WriteLine("Got xfer Ready"); };
+            _session.DataTransferred += (s, e) =>
+            {
+                // TODO 파일 경로에 대한 변수 참조가 언제 일어나는지? 해당 handler가 선언될때 생성되는지 혹은 호출될때 생성되는지
+                // TODO pdf naps2 참고
+                var stream = e.GetNativeImageStream();
+                var img = Image.Load(stream);
+                img.Save(_savePath);
+                Console.WriteLine(e.NativeData != IntPtr.Zero
+                    ? "SUCCESS! Got twain data"
+                    : "FAILED! No twain data");
+            };
+            _session.SourceDisabled += (s, e) =>
+            {
+                Console.WriteLine("Source disabled");
+                _session.CurrentSource.Close();
+                _session.Close();
+            };
+        }
+
+        public List<DataSource> getDataSourceList()
+        {
+            _session.Open();
+            var result = _session.GetSources().ToList();
+            _session.Close();
+            return result;
+        }
+
+        public string getSavePath()
+        {
+            return _savePath;
+        }
+
+        public void setSavePath(string newPath)
+        {
+            _savePath = newPath;
+        }
+    }
+}
